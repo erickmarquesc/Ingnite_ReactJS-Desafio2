98cff771654116b84227811dc3ac0542c87fd8d2
Erick Marques <erick.marquesc@hotmail.com>
2021-03-31 17:08:51 -0300

    Challenge Finalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from 'react';
-
 import { api } from './services/api';
-
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
-
 import './styles/global.scss';
 import './styles/sidebar.scss';
 import './styles/content.scss';
